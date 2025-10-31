b309305c3fd6be5c90dbaa5b52c18623e9cd4048
Jules Gantenbein <hamsolo4744@gmail.com>
2025-10-31 18:05:21 +1100

    Added SAP.py

---
+++ SAP.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import argparse
+
+SAP_path = r"SAP.csv"
+try:
+    df = pd.read_csv(SAP_path)
+except BaseException as e:
+    print('Cannot find SAP.csv, please contact v-micgilmore@microsoft.com for the sheet')
+    raise e
+
+op = []
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('terms',
+                        type=str,
+                        nargs='*',
+                        help='search terms')
+    args = parser.parse_args()
+
+    for i in df['Path']:
+        found = True
+        for j in args.terms:
+            if j.lower() not in str(i).lower():
+                found = False
+        if found == True:
+            op.append(i)
+    for i in sorted(set(op)):
+        print(i)
